8f13108a3bc6f24109e9559ff3a5de7d7450dd00
GustavoLuizOP <luizgusta212326@gmail.com>
2026-09-15 20:09:04 -0300

    criacao da logica inicial

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+
+function calcularTotal (itens) {let total = 0
+
+    for (let 1 = 0; i < itens.legth; i++){
+        total += itens[i].preco 
+    }
+
+    // aplica desconto de fidelidade
+    // antes de retornar o valor final
+
+    return total
+}
